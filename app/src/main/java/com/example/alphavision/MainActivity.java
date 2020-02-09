18a6bee6cff07ef6420f06f07841b4ea81995e0d
dexterai-lab <debabrata.rc91@gmaill.com>
2020-02-09 22:46:27 +0100

    Changed the Fragments implementation into Activity calls. Fixed the app theme error for toolbar content in Camera Activity

--- app/src/main/java/com/example/alphavision/MainActivity.java
+++ app/src/main/java/com/example/alphavision/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This feature will be added soon!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
