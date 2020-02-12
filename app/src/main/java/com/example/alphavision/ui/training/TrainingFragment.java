dcb6ed9b7451d221e51d50a1e571dd531f69b4d6
dexterai-lab <debabrata.rc91@gmaill.com>
2020-02-12 22:49:30 +0100

    Changed the Camera interface from take picture display to Active camera preview in the activity

--- app/src/main/java/com/example/alphavision/ui/training/TrainingFragment.java
+++ app/src/main/java/com/example/alphavision/ui/training/TrainingFragment.java
@@ -1,30 +1,32 @@
 package com.example.alphavision.ui.training;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.alphavision.CameraTakePictureActivity;
+import com.example.alphavision.DetectorActivity;
 import com.example.alphavision.R;
 
 import java.io.ByteArrayOutputStream;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -34,6 +36,7 @@ public class TrainingFragment extends Fragment {
     private Button buttonTakepic;
     private ImageView imageView;
     private TrainingViewModel trainingViewModel;
+    private String mCurrentPhotoPath;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -45,36 +48,86 @@ public class TrainingFragment extends Fragment {
         buttonTakepic = (Button) root.findViewById(R.id.buttonTakepic);
         imageView = (ImageView) root.findViewById(R.id.imageViewPicture);
 
+
         buttonTakepic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//                dispatchTakePictureIntent();
+//                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+                Intent intent = new Intent(getActivity(), CameraTakePictureActivity.class);
+                intent.putExtra("some","some data");
+                startActivity(intent);
             }
         });
 
         return root;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-
-            Bitmap bmp = (Bitmap) data.getExtras().get("data");
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            byte[] byteArray = stream.toByteArray();
-
-
-            Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
-                    byteArray.length);
-
-            imageView.setImageBitmap(bitmap);
-        }
-    }
-
-
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//
+//            File imgFile = new File(mCurrentPhotoPath);
+//            if (imgFile.exists()) {
+//                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+//                imageView.setImageBitmap(myBitmap);
+//            }
+//            Bundle extras = data.getExtras();
+//
+//            Bitmap bmp = (Bitmap) data.getExtras().get("data");
+//            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//
+//            bmp.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+//            byte[] byteArray = stream.toByteArray();
+//
+//
+//            Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+//                    byteArray.length);
+//
+//            imageView.setImageBitmap(bitmap);
+//        }
+//    }
 }
+
+//    private File createImageFile() throws IOException {
+//        // Create an image file name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        );
+//
+//        // Save a file: path for use with ACTION_VIEW intents
+//        mCurrentPhotoPath = image.getAbsolutePath();
+//        return image;
+//    }
+
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+//            // Create the File where the photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(this,
+//                        "com.example.android.fileprovider",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//            }
+//        }
+//    }
+
